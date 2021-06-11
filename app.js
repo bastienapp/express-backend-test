f0b69dd0a0ec450252ed9dc399492c53189d172f
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-06-11 12:12:15 +0200

    add route

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const authRoute = require('./routes/auth');
 
 const app = express();
 app.use(express.json());
 // TODO: configure cors
 app.use(cors());
 
-app.post('/signup', (request, response) => {
-  response.send(request.body);
-});
+app.use('/', authRoute);
 
 module.exports = app;
